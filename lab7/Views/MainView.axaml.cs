a9b47e2c4055487009fcf89cd1ddb3230324d06b
Kuba <pedzinskikuba@gmail.com>
2024-04-19 18:44:38 +0200

    Dodaj pliki projektów.

---
+++ lab7/Views/MainView.axaml.cs
@@ -0,0 +1,53 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Security;
+
+namespace lab7.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+    public void ButtonClicked(object source, RoutedEventArgs args)
+    {
+        Debug.WriteLine("Click!");
+
+        List<string> parts = new List<string>();
+        List<int> amount = new List<int>();
+        string result = "Wynik: \n";
+
+        for (int i = 0; i <= sekwencja.Text.Length-4; i++)
+        {
+            parts.Add(sekwencja.Text.Substring(i, 4));
+            amount.Add(1);
+        }
+        for (int i = 0; i < parts.Count; i++)
+        {
+            for (int j = i + 1; j < parts.Count; j++)
+            {
+                if (parts[i] == parts[j])
+                {
+                    amount[i]++;
+                    parts.RemoveAt(j);
+                    amount.RemoveAt(j);
+                    j--;
+                }
+            }
+        }
+
+        for (int i = 0; i < parts.Count; i++)
+        {
+            result += parts[i];
+            result += " - ";
+            result += amount[i].ToString();
+            result += "\n";
+        }
+        wynik.Text = result;
+    }
+}
